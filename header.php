0608ecacfeb05cc91546175f90117acfac978680
Arthur-MONNET <78175924+Arthur-MONNET@users.noreply.github.com>
2021-02-22 01:49:04 +0100

    change .html to .php

--- header.php
+++ header.php
@@ -15,13 +15,13 @@
 
 <nav class="flex flex-wrap items-center justify-between p-4">
     <div class="navbar-menu ">
-        <a class="block inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="/">
+        <a class="block inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="index.php">
             Accueil
         </a>
-        <a class="block inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="/realisations.html">
+        <a class="block inline-block mt-4 lg:mt-0 mr-10 text-blue-900 hover:text-indigo-600" href="realisations.php">
             Mes réalisations
         </a>
-        <a class="block inline-block mt-4 lg:mt-0 text-blue-900 hover:text-indigo-600" href="/contact.html">
+        <a class="block inline-block mt-4 lg:mt-0 text-blue-900 hover:text-indigo-600" href="contact.php">
             Contact
         </a>
     </div>
