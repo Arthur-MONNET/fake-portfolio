3146c30d492b88dcdd2ca61dd138a19db6aca5ca
Arthur-MONNET <78175924+Arthur-MONNET@users.noreply.github.com>
2021-02-22 22:46:37 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
-<h4 class="text-center text-sm my-20">© Marie-Louise Bourreau - <?php echo date("Y") ?></h4>
+<h4
+require "./data.php";
+class="text-center text-sm my-20">© <?php echo $user["name"] ?> - <?php echo date("Y") ?></h4>
 </body>
 </html>
