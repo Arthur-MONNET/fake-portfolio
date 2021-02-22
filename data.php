1542aa310ed8bbc3eb12036db7bd47d81653f327
Arthur-MONNET <78175924+Arthur-MONNET@users.noreply.github.com>
2021-02-22 03:06:10 +0100

    Update data.php

--- data.php
+++ data.php
@@ -33,7 +33,7 @@ $user = [
 ];
 
 
-$formation = [
+$formations = [
     [
         "date" => "2017/2019",
         "formation" => "BDDI1",
@@ -43,7 +43,7 @@ $formation = [
 ];
 
 
-$passion = [
+$passions = [
     [
         "passion" => "Photographie",
         "description" => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
